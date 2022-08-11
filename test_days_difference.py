01ead3ea2babf32dbfe04583657b64373e44a5d2
Lenka Wolfova <lenka.wolfova@grhapp.com>
2022-08-11 10:58:24 +0100

    adding corrected test file

--- test_days_difference.py
+++ test_days_difference.py
@@ -4,12 +4,30 @@ from datecalc import when # importing function from the backend file which needs
 from datetime import datetime, timedelta # importing datetime module
 
 def test_for_days_difference():
-  d1 = datetime(2022,8,2)
-  d2 = datetime(2021,7,6)
-  assert duration(d1,d2) == 392
+#checking if the returned value is correct when inputting the start date and end date
+
+  # Arrange
+  end_date = datetime(2022,8,2)
+  start_date = datetime(2021,7,6)
+  expected_result = 392
+
+  # Act
+  final_act = abs((end_date - start_date).days)
+
+  # Assert
+  assert final_act == expected_result
+
 
 def test_for_date_since_start_date():
 #checking if the returned date is correct when inputting the start date and duration
-  d1 = datetime(2022,8,9)
-  dur = int(5)
-  assert when(d1,dur) == datetime(2022,8,14)
+
+  # Arrange
+  start_date = datetime(2022,8,9)
+  days_between = int(5)
+  expected_result = datetime(2022,8,14)
+
+  # Act
+  final_act = start_date + timedelta(days=days_between)
+
+  #Assert
+  assert final_act == expected_result
